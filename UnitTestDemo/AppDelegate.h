b08b201f23b1ed2bded403de4566b7939db780bc
hechao <hechao@hechaodeMac-mini.local>
2018-12-27 09:51:16 +0800

    Initial Commit

---
+++ UnitTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnitTestDemo
+//
+//  Created by hechao on 2018/12/27.
+//  Copyright © 2018 hechao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
