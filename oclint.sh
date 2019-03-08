75c56d9943c7432a4bcf092e48a8538a13ec0c48
hechao <1054134542@qq.com>
2019-03-08 17:03:06 +0800

    修改规则

--- oclint.sh
+++ oclint.sh
@@ -9,13 +9,14 @@ xcodebuild -workspace $myworkspace -scheme $myscheme \
 -configuration Debug CLANG_ENABLE_MODULE_DEBUGGING=NO COMPILER_INDEX_STORE_ENABLE=NO \
 | xcpretty -r json-compilation-database -o compile_commands.json&&
 oclint-json-compilation-database -e Pods -- \
--report-type html -o oclint_result.html \
--rc LONG_LINE=200 \
+-report-type pmd -o oclint_result.xml \
+-rc LONG_LINE=400 \
+-rc LONG_METHOD=200 \
 -max-priority-1=100000 \
 -max-priority-2=100000 \
 -max-priority-3=100000; \
 rm compile_commands.json;
-if [ -f ./oclint_result.html ]; 
+if [ -f ./oclint_result.xml ]; 
 then echo 'done'
 else echo "failed";
 fi
